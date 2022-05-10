24f93ba86ee79c35439dfc48f073c9d9d00882b0
jyldyzbek00 <jyldyzbek.sayak@gmail.com>
2022-05-10 20:47:28 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector( ".header-btn")
+btn.addEventListener( "click", () =>{
+    document.querySelector( ".header-square")
+    .style.display = "block"
+})
+
+const closeBtn = document.querySelector( ".header-close-btn")
+closeBtn.addEventListener("click",() =>{
+    document.querySelector("header-square")
+        .style.display = "none"
+})
+
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
